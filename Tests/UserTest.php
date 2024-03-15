79d1246a5f3c02e5ad5906b3323a4b05d10c0356
L9029 <angeloca03@gmail.com>
2024-03-15 16:14:41 -0400

    Improving the UserTest class with the validation of the user data and Developing the validation method for the User class.

--- Tests/UserTest.php
+++ Tests/UserTest.php
@@ -8,14 +8,13 @@ use App\Author;
 class UserTest extends TestCase{
 
     public function test_author_data(){
-        $user_profile = new User("Guest", "Guest9029", 9029);
+        $user_profile = new User("<h1>hoa</h1>", "Guest9029", "9029");
+        $user_profile->validate_sanitizeUserData();
 
-        $user_profile->getUserData();
-
-        //Change the asserts
-        $this->assertIsString($user_profile->getUserData()[0]);
-        $this->assertIsString($user_profile->getUserData()[1]);
-        $this->assertIsInt($user_profile->getUserData()[2]);
+        //Validation and Sanizitacion data Asserts from the User Parameters
+        for ($i=0; $i < 3; $i++) { 
+            $this->assertTrue($user_profile->validate_sanitizeUserData()[$i]);
+        }
     }
 }
 
--- src/User.php
+++ src/User.php
@@ -13,7 +13,16 @@ class User{
         $this->user_age = $user_age;
     }
 
-    //Create a Validation for the user data here
+    //Validation and Sanitize for the user data
+    public function validate_sanitizeUserData(){
+        $htmlentities_full_name_convert_to_str = is_string(htmlentities($this->full_name));
+        $htmlentities_username_convert_to_str = is_string(htmlentities($this->user_name));
+        $filter_age_user = is_int(filter_var($this->user_age, FILTER_VALIDATE_INT));
+
+        return [$htmlentities_full_name_convert_to_str, $htmlentities_username_convert_to_str, $filter_age_user];
+    }
+
+    //Get the User Data
     public function getUserData(){
         return [$this->full_name, $this->user_name, $this->user_age];
     }
