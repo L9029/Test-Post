e635f077a5c7f7950e17ac782f4e4ceb34f56d2c
L9029 <angeloca03@gmail.com>
2024-03-18 14:42:07 -0400

    Adding some validation tests to the PostTest class, creating the methods of the Post class to set the title, category and content to the post.

--- Tests/PostTest.php
+++ Tests/PostTest.php
@@ -6,11 +6,32 @@ use PHPUnit\Framework\TestCase;
 //Calling the class Post and Comment from the App
 use App\Post;
 use App\Comment;
+use App\Category;
 
 //Creating the Test class for the Post class
 class PostTest extends TestCase{
 
     //Test add_content_to_post
+    public function test_add_content(){
+        $post = new Post();
+        $content = $post->addContent("Unit Test for adding content to the post");
+
+        $this->assertIsString($content);
+    }
+
+    public function test_set_title(){
+        $post = new Post();
+        $post_title = $post->setTitle("Unit Test");
+
+        $this->assertIsString($post_title);
+    }
+
+    public function test_set_category(){
+        $post = new Post();
+        $category = new Category();
+
+        $this->assertInstanceOf(Category::class, $post->setCategory($category));
+    }
 
     //This method validate if the comment object is an instance from the class Comment
     public function test_add_comment_to_post(){
--- src/Post.php
+++ src/Post.php
@@ -3,7 +3,34 @@
 namespace App;
 
 class Post{
+    protected $title;
+    protected $category;
+    protected $content;
     protected $comments = [];
+
+    //Set the title of the content
+    public function setTitle(string $title){
+        return $this->title = $title;
+    }
+
+    //Set the Category of the content
+    public function setCategory(Category $category){
+        return $this->category = $category;
+    }
+
+    //Add the content for the post
+    public function addContent(string $content){
+        return $this->content = $content;
+    }
+
+    //Get all the content of the post with all the caracteristics
+    public function getContent(){
+        $this->title = $title;
+        $this->category = $category;
+        $this->content = $content;
+
+        return $this->category;
+    }
 
     //the parameter Comment in the function validate that the comment var has to be the Comment type
     public function addComment(Comment $comment){
